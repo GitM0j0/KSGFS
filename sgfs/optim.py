a43d56a8cf4a6742369f25d962b1ee928926896e
GitM0j0 <seiler.moritz@gmail.com>
2018-07-10 15:45:43 +0100

    Update optim.py

--- sgfs/optim.py
+++ sgfs/optim.py
@@ -8,7 +8,7 @@ from torch.distributions import Normal
 import numpy as np
 
 
-class sgld(object):
+class sgfs(object):
     def __init__(self,net, B, a, b, gamma, lambda_, batch_size, dataset_size):
         self.net = net
         self.n = batch_size
@@ -25,7 +25,7 @@ class sgld(object):
             weight_grad = l.weight.grad
             mean_weight_grad = torch.mean(weight_grad,0)
             diff_grad = weight_grad - mean_weight_grad
-            cov_grads = 1 / n * diff_grad.transpose(1,0).mm(diff_grad)
+            cov_grads = 1 / (self.n - 1) * diff_grad.transpose(1,0).mm(diff_grad)
 
             #weight_grad.add_(self.lambda_, l.weight.data)
             # Exponential LR decay
@@ -35,20 +35,20 @@ class sgld(object):
 
             I_hat = (1 - 1. / epoch) * I_hat + 1. / epoch * cov_grads
 
+            mat = self.gamma * I_hat + (4. / learning_rate) * self.B
+            l, u = torch.symeig(mat)
+            mat_inv = (u * ((l + ) ** (-1))).mm(u.transpose(1,0))
+            ### INVERSE
+
             size=weight_grad.size()
-#            noise = Normal(
-#                torch.zeros(size),
-#                torch.ones(size) * np.sqrt(learning_rate)
-#            )
 
             noise = Normal(
                 torch.zeros(size),
-                torch.ones(size) * np.sqrt(learning_rate)
+                torch.ones(size) * np.sqrt((4. /learning_rate) * self.B)
                 )
             # Mini-batch updates
-            # theta_(t+1) = theta_t - eta_t * 0.5 *( grad(log p(theta_t)) + N/n sum(grad(log p(x_t|theta_t)))) + eta_t
-            # with eta_t ~ N(0, eta_t)
+            # theta_(t+1) = theta_t + 2 * (gamma * I_hat + N * grad_avg(theta_t; X_t))^⁻1 * ( grad(log p(theta_t)) + N * grad_avg(theta_t) + eta_t)
+            # with eta_t ~ N(0, 4 * B / eta_t)
 
-            #update = learning_rate * 0.5 * self.N / self.n * weight_grad + noise.sample()
-            update = learning_rate * 0.5 * weight_grad + noise.sample()
-            l.weight.data.add_(-update)
+            update = mat_inv.mm(2. * (self.N * mean_weight_grad).add_(self.lambda_, l.weight.data) + noise.sample())
+            l.weight.data.add_(update)

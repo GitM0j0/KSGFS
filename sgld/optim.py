a5cdf0ed394f2798c9766ef883e0fdc48af2954d
seiler.moritz@gmail.com <seiler.moritz@gmail.com>
2018-07-11 17:46:04 +0100

    updated

--- sgld/optim.py
+++ sgld/optim.py
@@ -3,13 +3,12 @@ import torch
 import torchvision
 
 from torch import nn
-from torch.distributions import Normal
 
 import numpy as np
 
 
 class sgld(object):
-    def __init__(self,net, a, b, gamma, lambda_, batch_size, dataset_size):
+    def __init__(self,net, a, b, gamma, tau, batch_size, dataset_size):
         self.net = net
         self.n = batch_size
         self.N = dataset_size
@@ -19,29 +18,36 @@ class sgld(object):
         self.b = b
         self.gamma = gamma
         #self.lr_decayEpoch = lr_decayEpoch
-        self.lambda_ = lambda_
+        self.tau = tau
 
     def step(self, epoch=0):
         for l in self.linear_layers:
             weight_grad = l.weight.grad
 
-            grad_logPost = (weight_grad).add_(self.lambda_, l.weight.data / self.N)
+            # Mini-batch updates in Ahn et al. (2012)
+            # theta_(t+1) = theta_t - epsilon_t * 0.5 *( grad(log p(theta_t)) + N/n sum(grad(log p(x_t|theta_t)))) + eta_t
+            # with eta_t ~ N(0, epsilon_t)
+
+
+            # According to Ahn et al. (2012) without averaging (original paper) - NOT WORKING!!!
+            #grad_logPost = (float(self.N) / self.n * weight_grad).add_(self.tau, l.weight.data)
+            #learning_rate = self.a * (self.b + epoch) ** (-self.gamma)
+            #noise = torch.randn_like(weight_grad) * (learning_rate) ** (-0.5)
+            #update = (learning_rate * 0.5 * grad_logPost).add_(noise)
+
+            # According to Marceau-Caron/Ollivier (2017) with averaging - WORKS!
+            # Uses modified learning_rate lr = 2 * epsilon / N
+            grad_logPost = (1. / self.n * weight_grad).add_(self.tau, l.weight.data / self.N)
             # Exponential LR decay
-            #learning_rate = self.lr_init * (2**(-epoch // self.lr_decayEpoch))
+            # According to Ahn et al. (2012)
             learning_rate = self.a * (self.b + epoch) ** (-self.gamma)
+            # According to Li et al.(2016) - Modification of required parameters!
+            #learning_rate = self.lr_init * (2**(-epoch // self.lr_decayEpoch))
 
-            noise = torch.randn_like(weight_grad) * learning_rate ** (-0.5)
+            noise = torch.randn_like(weight_grad) * (2. * learning_rate / self.N)
 
-            # size = weight_grad.size()
-            # noise = Normal(
-            #     torch.zeros(size),
-            #     torch.ones(size) * learning_rate ** (-0.5)
-            #     )
-            # Mini-batch updates
-            # theta_(t+1) = theta_t - eta_t * 0.5 *( grad(log p(theta_t)) + N/n sum(grad(log p(x_t|theta_t)))) + eta_t
-            # with eta_t ~ N(0, eta_t)
 
-            #update = learning_rate * 0.5 * self.N / self.n * weight_grad + noise.sample()
+
+            update = (learning_rate * 0.5 * grad_logPost).add_(noise)
             #update = (learning_rate * 0.5 * grad_logPost).add_(2 * noise.sample() / self.N)
-            update = (learning_rate * 0.5 * grad_logPost).add_(2 * noise / self.N)
             l.weight.data.add_(-update)
--- train_sgld.py
+++ train_sgld.py
@@ -14,20 +14,6 @@ from sgld import model, optim
 
 import mnist
 
-### Utils
-
-#def log_gaussian(x, precision):
-#    # Normal distribution N(0, 1/precision)
-#    return -0.5 * (torch.log(torch.tensor(2.) * np.pi / precision) + precision * x.pow(2))
-#
-#def gaussian_prior(x, precision):
-#    res = 0
-#    for p in x:
-#        res += torch.sum(log_gaussian(p, precision))
-#    return res
-
-
-###
 
 
 if torch.cuda.is_available():
@@ -53,28 +39,24 @@ dataset_size = len(train_loader.dataset)
 
 
 network = model.shallow_network()
-criterion = nn.CrossEntropyLoss(size_average=True)
+criterion = nn.CrossEntropyLoss(size_average=False)
 
 #optim = sgld_alt.optim.sgld(network, lr, weight_decay, lr_decayEpoch, batch_size, dataset_size)
 optim = optim.sgld(network, a, b, gamma, weight_decay, batch_size, dataset_size)
 
-for epoch in range(10):
+for epoch in range(20):
     running_loss = 0
     for x, y in iter(train_loader):
         x = x.view(x.size(0), -1)
 
         network.zero_grad()
         output = network(x)
-        #log_likelihood = criterion(output, target)
-        #log_prior = sum([torch.sum(gaussian_prior(p, precision)) for p in network.parameters()])
-        #loss = log_prior - train_data.train_data.size(0) / batch_size * log_likelihood
         loss = criterion(output, y)
         loss.backward()
         optim.step(epoch)
 
         running_loss += loss * batch_size / dataset_size
-        #running_loss += loss
-        prediction = output.data.max(1)[1]   # first column has actual prob.
+        prediction = output.data.max(1)[1]
         accuracy = torch.sum(prediction.eq(y)).float()/batch_size
 
     print("Epoch {:d} - loss: {:.4f} - acc: {:.4f}".format(epoch, running_loss, accuracy))
@@ -85,7 +67,7 @@ for epoch in range(10):
             x = x.view(x.size(0), -1)
             output = network(x)
             test_metric += 100 * (output.argmax(1) == y).float().sum() / len(test_loader.dataset)
-            prediction = output.data.max(1)[1]   # first column has actual prob.
+            prediction = output.data.max(1)[1]   
             accuracy = torch.sum(prediction.eq(y)).float()/1000
 
         print("\ttest: {:.4}  - acc: {:.4}".format( test_metric, accuracy))

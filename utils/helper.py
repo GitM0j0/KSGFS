b90be987ddea51b213bee263ac9b587c5455ed67
GitM0j0 <seiler.moritz@gmail.com>
2018-07-20 15:15:14 +0100

    Update helper.py

--- utils/helper.py
+++ utils/helper.py
@@ -17,3 +17,18 @@ def goodfellow(model, X, y):
 	gradients = goodfellow_backprop(activations, linearGrads)
 	
 	return gradients
+
+
+def goodfellow_backprop(activations, linearGrads):
+	grads = []
+	for i in range(len(linearGrads)):
+		G, X = linearGrads[i], activations[i]
+		if len(G.shape) < 2:
+			G = G.unsqueeze(1)
+		
+		G *= G.shape[0] # if the function is an average
+		
+		grads.append(torch.bmm(G.unsqueeze(2), X.unsqueeze(1)))
+		grads.append(G)
+
+	return grads

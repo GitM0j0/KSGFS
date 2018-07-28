191493d1858a1f1d76d34d7986c6d9799060819c
seiler.moritz@gmail.com <seiler.moritz@gmail.com>
2018-07-28 19:08:39 +0100

    updated

--- sgfs/optim.py
+++ sgfs/optim.py
@@ -1,6 +1,7 @@
 # Load required libraries
 import torch
 import torchvision
+import math
 
 from torch import nn
 from torch.distributions import Normal
@@ -9,19 +10,21 @@ import numpy as np
 
 
 class sgfs(object):
-    def __init__(self,net, epsilon,lambda_, batch_size, dataset_size):
-        self.net = net
+    def __init__(self,network, epsilon,lambda_, batch_size, dataset_size):
+        self.network = network
         self.n = batch_size
         self.N = dataset_size
         self.epsilon =epsilon
         self.gamma = np.float(dataset_size + batch_size) / batch_size
-        self.linear_layers = [m for m in self.net.modules() if isinstance (m, nn.Linear)]
+        self.linear_layers = [m for m in self.network.modules() if isinstance (m, nn.Linear)]
         #self.lr_init = lr
         #self.lr_decayEpoch = lr_decayEpoch
         self.lambda_ = lambda_
         self.t = 1
         self.I_hat = dict()
         self.grad_mean = dict()
+        #self.learning_rate = 2. / (self.gamma + (4. / epsilon))
+        self.noise_factor = 2 * math.sqrt(self.gamma / (epsilon * self.N))
 
 
         self.grads_per_element = dict()
@@ -35,26 +38,28 @@ class sgfs(object):
             if len(G.shape) < 2:
                 G = G.unsqueeze(1)
 
-            #G *= G.shape[0] # if the function is an average
+            G *= G.shape[0] # if the function is an average
 
             self.grads_per_element[l] = torch.bmm(G.unsqueeze(2), X.unsqueeze(1))
 
 
     def emp_fisher(self):
         for l in self.linear_layers:
+
             self.grad_mean[l] = self.grads_per_element[l].sum(0) / self.n
             diff_grads = self.grads_per_element[l] - self.grad_mean[l]
-            scale = (self.n - 1) ** (-1)
+            scale = (self.n - 1) ** -1
 
             # SGFS-f update
             cov_scores = torch.einsum('bij,bkj->ik', (diff_grads, diff_grads)) * scale
+
             # SGFS-d update
-            # cov_scores = cov_scores.mul(torch.eye(cov_scores.size(0)))
+            #cov_scores = cov_scores.mul(torch.eye(cov_scores.size(0)))
 
             if self.t == 1:
                 self.I_hat[l] = torch.zeros(l.weight.data.size(0),l.weight.data.size(0))
             else:
-                self.I_hat[l] = (1 - 1. / self.t) * self.I_hat[l] + (1. / self.t) * cov_scores
+                self.I_hat[l] = ((1 - 1. / self.t) * self.I_hat[l]).add((1. / self.t) * cov_scores)
 
 
 
@@ -68,23 +73,24 @@ class sgfs(object):
 
             # According to Ahn et al. (2012): B \propto N*I_hat
             # Porbably scale problem here!!!
-            if self.t < 50:
-                B = torch.eye(self.I_hat[l].size(0))
-            else:
-                B = self.N* self.I_hat[l]
-
-            # Update of precoditioner
-            mat = self.gamma * self.N * self.I_hat[l]  + (4. / self.epsilon * B)
-            mat_inv = mat.inverse()
+            # if self.t < 10:
+            #     I_hat_inv = torch.eye(self.I_hat[l].size(0))
+            # else:
+            eps = 1e-8 * 10 ** -(self.t // 10)
+            B = self.gamma * self.I_hat[l]
+            mat = self.gamma * self.I_hat[l] + 4. * B / self.epsilon
+            mat_inv = torch.inverse(mat.add(torch.eye(self.I_hat[l].size(0))))
+            #mat_inv = torch.inverse(mat)
 
             # Cholesky factor of matrix B
-            B_ch = torch.potrf(B)
 
-            noise = (2. * (self.N * self.epsilon) ** (-0.5) * B_ch).mm(torch.randn_like(self.grad_mean[l]))
+            B_ch = torch.potrf(B.add(eps, torch.eye(self.I_hat[l].size(0))), upper=True)
+            #B_ch = torch.potrf(B, upper=False)
 
+            noise = (self.noise_factor * B_ch).mm(torch.randn_like(self.grad_mean[l]))
 
             # Update in parameter space
-            update = mat_inv.mm(2. * (mean_weight_grad).add_(self.lambda_ / self.N, l.weight.data).add_(noise))
-
+            update = 2. * (mat_inv).mm((self.grad_mean[l]).add_(self.lambda_ / self.N, l.weight.data))#.add_(noise))
             l.weight.data.add_(-update)
+            #print(update)
         self.t += 1
--- train_sgfs.py
+++ train_sgfs.py
@@ -1,6 +1,10 @@
 import torch
+import torchvision
+
+import os
 
 from torch import nn
+from torchvision import transforms
 import torch.nn.functional as F
 
 
@@ -19,23 +23,30 @@ if torch.cuda.is_available():
 else:
     device = torch.device("cpu")
 
-batch_size = 500
+batch_size = 2000
 #lr = 1e-5
 #lr_decayEpoch = 20
 num_workers = 5
 
 lambda_ = 0.001
-epsilon =  2.
+epsilon =  1.
 
+train_data = torchvision.datasets.MNIST(root=os.environ.get("DATASETS_PATH", "~/datasets"), train=True,
+                                         download=True, transform=transforms.ToTensor())
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, num_workers=5)
 
-train_loader, test_loader = mnist.get_mnist(batch_size, num_workers)
+test_data = torchvision.datasets.MNIST(root=os.environ.get("DATASETS_PATH", "~/datasets"), train=False,
+                                        download=True, transform=transforms.ToTensor())
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=1000)
 
 train_size = 60000
 test_size = 10000
 
 # model_sgfs: preactivation and activation matrices required
-network = model_sgfs.mlp([784,400,400,10])
+network = model_sgfs.mlp([20,30,10],nl=F.sigmoid)
 criterion = F.binary_cross_entropy_with_logits
+
+random_matrix = torch.randn(784,20)
 
 
 optim = optim.sgfs(network, epsilon, lambda_, batch_size, train_size)
@@ -44,10 +55,11 @@ for epoch in range(10):
     running_loss = 0
     for x, y in iter(train_loader):
         x = x.view(x.size(0), -1)
+        x_projection = x.mm(random_matrix)
         one_hot = torch.zeros(batch_size,10).scatter_(1,y.view(-1,1).long(),1)
 
         network.zero_grad()
-        output, nonLinearities, preactivations = network(x)
+        output, nonLinearities, preactivations = network(x_projection)
         loss = criterion(output, one_hot)
         preactivation_grads = torch.autograd.grad(loss, preactivations)
         # according to Goodfellow (see optim.py)
@@ -68,7 +80,8 @@ for epoch in range(10):
         test_metric = 0
         for x, y in iter(test_loader):
             x = x.view(x.size(0), -1)
-            output, _, _ = network(x)
+            x_projection = x.mm(random_matrix)
+            output, _, _ = network(x_projection)
             test_metric += 100 * (output.argmax(1) == y).float().sum() / test_size
 
 

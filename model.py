191c9ba0d49ba4074db1583cd77c28c8056a20e8
seiler.moritz@gmail.com <seiler.moritz@gmail.com>
2018-08-01 11:23:19 +0100

    layer added

--- model.py
+++ model.py
@@ -18,7 +18,25 @@ class shallow_network(nn.Module):
                       nn.ReLU(),
                       nn.Linear(400, 400, bias=False),
                       nn.ReLU(),
+                      nn.Linear(400, 400, bias=False),
+                      nn.ReLU(),
                       nn.Linear(400,10, bias=False)
+                    ).to(device)
+
+    def forward(self, x):
+        x = x.view(x.size(0), -1)
+        x = self.model(x)
+        return x
+
+class network_projected(nn.Module):
+    def __init__(self):
+        super(network_projected, self).__init__()
+        self.model = nn.Sequential(
+                      nn.Linear(20, 30, bias=False),
+                      nn.ReLU(),
+                      nn.Linear(30, 30, bias=False),
+                      nn.ReLU(),
+                      nn.Linear(30, 10, bias=False)
                     ).to(device)
 
     def forward(self, x):
